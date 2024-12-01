bee8cb08ee74fa56fe09a1e193da156ba8ed9c2a
Adam Bonsu <adam@bonsu.io>
2024-12-01 23:23:35 +0000

    update IOSTest to use new appiumConfig remote url interface

--- src/test/java/com/adambonsu/apps/greetinghexjavascriptreact/tests/IOSTest.java
+++ src/test/java/com/adambonsu/apps/greetinghexjavascriptreact/tests/IOSTest.java
@@ -3,6 +3,8 @@ package com.adambonsu.apps.greetinghexjavascriptreact.tests;
 import org.testng.annotations.BeforeClass;
 
 import com.adambonsu.apps.greetinghexjavascriptreact.base.BaseTest;
+
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 
@@ -14,11 +16,23 @@ public class IOSTest extends BaseTest {
 
         XCUITestOptions options = new XCUITestOptions()
             .setDeviceName(System.getenv("DEVICE_NAME"))
+            .setPlatformVersion(System.getenv("DEVICE_OS_VERSION"))
+            .setUdid(System.getenv("DEVICE_UDID"))
             .setAutomationName("xcuitest")
             .setApp(System.getenv("APP_PATH"))
             .setPlatformName("iOS");
 
-        driver = new IOSDriver(appiumConfig.getServerURL(), options);
+        logger.debug("Configuring driver..." + options);
+
+        try{
+            driver = new IOSDriver(appiumConfig.getRemoteServerURL(), options);
+        } catch (Exception e) {
+            logger.error("Error configuring driver: " + e.getMessage());
+            throw e;
+        }
+
+        logger.debug("Configuring driver timeouts...");
         configureDriverTimeouts();
+        logger.info("Set up completed");
     }
 }
