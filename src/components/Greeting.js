6c540502d1081879bdb995b172d6104117f6b7b0
Adam Bonsu <adam@bonsu.io>
2025-01-07 09:20:59 +0000

    referencing core package

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {greet} from '@adambonsu/greeting-hex-javascript';
 
 const Greeting = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text} accessibilityLabel="greeting" testID="greeting">
-        Hello Worldo!
+        {greet()}
       </Text>
     </View>
   );
