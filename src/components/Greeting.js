a433796fddb05a5291a5b43a23b87ff6b2a1f9a3
Adam Bonsu <adam@bonsu.io>
2025-01-11 17:39:01 +0000

    initial o11y tracing for greeting

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,34 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {greet} from '@adambonsu/greeting-hex-javascript';
-import {
-  SimpleSpanProcessor,
-  WebTracerProvider,
-} from '@opentelemetry/sdk-trace-web';
-import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-http';
-import {trace} from '@opentelemetry/api';
-
-const setupTracing = operationName => {
-  const provider = new WebTracerProvider();
-  const exporter = new OTLPTraceExporter({
-    url: 'https://<otel-collector-ip>:4318/v1/traces',
-  });
-  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-  provider.register();
-  return trace.getTracer(operationName);
-};
-
-const useTracing = operationName => {
-  const tracer = setupTracing('greeting-component');
-  const [span, setSpan] = useState(null);
-
-  useEffect(() => {
-    const newSpan = tracer.startSpan(operationName);
-    setSpan(newSpan);
-    return () => newSpan.end();
-  }, [operationName]);
-  return span;
-};
+import useTracing from './hooks/useTracing';
 
 const Greeting = () => {
   const [greeting, setGreeting] = useState('');
